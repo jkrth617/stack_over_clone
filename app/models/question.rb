5ea0801b063fb17029e5c976f18cb1ffa196379a
smokecode <smokecode@gmail.com>
2015-10-22 16:23:47 -0400

    model relations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ActiveRecord::Base
+  has_many :answers, dependent: :destroy
+  belongs_to :user
+  has_many :question_tags, dependent: :destroy
+  has_many :votes, as: :voteable, dependent: :destroy
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,3 @@
 class Tag < ActiveRecord::Base
+  has_many :question_tags, dependent: :destroy
 end
