ccaddf7508ddb26c6db106c65a2ab2c994c042c3
smokecode <smokecode@gmail.com>
2015-10-22 16:42:18 -0400

    vote value for question and answer

--- db/migrate/20151022192949_create_votes.rb
+++ db/migrate/20151022192949_create_votes.rb
@@ -2,6 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.integer :user_id, null:false
+      t.integer :value, default: 1
       t.references :voteable, polymorphic: true, index:true
       t.timestamps
     end
