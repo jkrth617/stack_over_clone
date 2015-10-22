26e7024cb2a1f3be46171921edcb500da95121dc
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-22 17:27:28 -0400

    add user#create action

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,21 @@ class UserController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Couldn't register user" #could change this to use flash errors.full_messages
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
