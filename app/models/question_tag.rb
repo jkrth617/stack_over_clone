9ee807523d6ba4c80cc5535efe98b5ad2d6ec908
smokecode <smokecode@gmail.com>
2015-10-22 16:37:39 -0400

    implement validation

--- app/models/question_tag.rb
+++ app/models/question_tag.rb
@@ -1,4 +1,6 @@
 class QuestionTag < ActiveRecord::Base
   belongs_to :question
   belongs_to :tag
+
+  validates_presence_of :tag_id, :question_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
   has_many :votes
+
+  validates_presence_of :username, :email, :password_digest
+  validates_uniqueness_of :email, :username
+
+  #validates :email, format: { }
 end
