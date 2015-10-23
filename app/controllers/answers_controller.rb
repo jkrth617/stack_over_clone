53d75c818898c08100f40144b331eb10f1fc73ef
AgentWang007 <jaybwang007@gmail.com>
2015-10-23 12:01:37 -0400

    update answers controller with show, create, and answer_params

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,20 @@
 class AnswersController < ApplicationController
+
+  def show
+    @question = Question.find(params[:question_id])
+  end
+
+  def create
+    question = Question.find(params[:question_id])
+    @answer = Answer.new(answers_params)
+    if @answer.save
+    else
+    end
+    redirect_to question
+  end
+
+  def answers_params
+    params.require(:answer).permit(:body, :user_id, :question_id)
+  end
+
 end
