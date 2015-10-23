a901b0a47b770e6cb9630690b64748aefec4aa64
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-23 15:23:47 -0400

    add test for user create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to be_successful
     end
   end
+
+  context "#create" do
+    let(:user) { FactoryGirl.create :user }
+    it 'redirects back to root path after user creation' do
+      post :create, {:user => FactoryGirl.attributes_for(:user)}
+      expect(response).to redirect_to root_path
+    end
+  end
 end
