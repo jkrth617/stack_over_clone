a57c36a1a2e9c0f41d0ad80d074e12146330d98a
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-23 16:30:25 -0400

    add specs for sessions

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe SessionsController, type: :controller do
       expect(response).to be_success
     end
   end
+
+  context "#create" do
+    context "With valid credentials" do
+      let :credentials do
+        {:email => "bob@bob.com", username: "bob", password: "burgers" }
+      end
+     let :user do
+       FactoryGirl.create(:user, credentials)
+     end 
+
+     before :each do
+      post '/login', credentials
+     end
+
+     it 'creates a user session' do
+      session[:user_id].should == user.id 
+     end
+    end
+  end
 end
