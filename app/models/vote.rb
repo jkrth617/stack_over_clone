455354b02070ee6ea3f9d53d4b2f84319acbe837
jake roth <email": null>
2015-10-23 16:08:24 -0400

    edited the vote class method

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,10 +5,11 @@ class Vote < ActiveRecord::Base
   validates_presence_of :value
 
   def value_check
-    prev_vote = Vote.where(user_id: slef.user_id, question_id: self.question_id)#where silently fails
-    if !(prev_vote.empty?)
+    prev_vote = Vote.find_by(user_id: self.user_id, voteable: self.voteable)#where silently fails
+    if prev_vote
       prev_vote.value += self.value unless prev_vote.value == self.value
-      self = prev_vote
+      return prev_vote
     end
+    self
   end
 end
