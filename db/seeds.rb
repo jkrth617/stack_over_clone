397375d11b1bfab2ca27bff00a10372b726273c0
jake roth <email": null>
2015-10-23 16:43:36 -0400

    added seed the view for the index is a little weird

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "coolkid", email:"test@example.com", password:"password")
+User.create(username: "shaq", email:"test2@example.com", password:"password")
+
+Question.create(user_id:1, title: "is jason too into toilets???", body: "I mean the kid does seem a little weird about the whole thing....")
+Answer.create(body: "seems like it", user_id:2, question_id:2)
+
+
+4.times do
+  q = Question.create(user_id:1, title: Faker::Hacker.ingverb+"?", body: Faker::Hacker.say_something_smart+"???")
+  4.times do
+    Answer.create(question_id:q.id, user_id:2, body: Faker::Hacker.say_something_smart+"!")
+  end
+end
