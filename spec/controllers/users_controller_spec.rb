65ef48c72bdbf3031932f38ed264b368f456ef67
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-23 14:15:07 -0400

    add :new template test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  context "#new" do
+    it 'response should be successful' do
+      get :new
+      expect(response).to be_successful
+    end
+  end
 end
