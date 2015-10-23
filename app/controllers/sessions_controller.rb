8836219c058320c8275ef3a27d28a7b0a479053e
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-23 11:16:26 -0400

    add sessions create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:message] = "Successfully logged in"
+      redirect_to root_path
+    else
+      flash.now.alert = "Mismatched username/password"
+      render :new
+    end
+  end
 end
