17851a00ee86d1669662070525b9821769d30b58
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-23 14:23:20 -0400

    add factory for creating users

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    username { Faker::Internet.user_name }
+    password { Faker::Internet.password } 
+    email    { Faker::Internet.email } 
+  end
+end
