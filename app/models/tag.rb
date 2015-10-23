f34b7ba30b3b15a6f4ef73f44664c47d6bd3aaa3
Ersan <smokecode@users.noreply.github.com>
2015-10-23 19:54:18 -0400

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,12 +2,4 @@ class Tag < ActiveRecord::Base
   has_many :question_tags, dependent: :destroy
   has_many :questions, through: :question_tags
   validates_presence_of :description
-
-  def tag_params
-    params.require(:tag).permit(:description)
-  end
-
-  def self.make_tags
-    tag_params.split.map { |desc| Tag.where(description: desc).first_or_create }
-  end
 end
