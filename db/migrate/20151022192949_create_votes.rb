0095020df2ab19c9598cbdc7e65df6a8edfdff96
jake roth <email": null>
2015-10-23 18:16:59 -0400

    changed the default

--- db/migrate/20151022192949_create_votes.rb
+++ db/migrate/20151022192949_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.integer :user_id, null:false
-      t.integer :value, default: 1
+      t.integer :value, default: 0
       t.references :voteable, polymorphic: true, index:true
       t.timestamps
     end
