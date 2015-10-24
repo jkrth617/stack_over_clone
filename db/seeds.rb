2b8035b9f28d44ed4dd90a90e525612b99aa89d7
smokecode <smokecode@gmail.com>
2015-10-24 19:31:32 -0400

    1000 questions with 51 random users - some of them shaq

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(username: "coolkid", email:"test@example.com", password:"password")
-User.create(username: "shaq", email:"test2@example.com", password:"password")
+50.times do
+  name = Faker::Name.first_name
+  email = Faker::Internet.email(name)
 
-Question.create(user_id:1, title: "is jason too into toilets???", body: "I mean the kid does seem a little weird about the whole thing....")
-Answer.create(body: "seems like it", user_id:2, question_id:1)
+  User.create(username: name, email: email, password:"password")
+end
+User.create(username: "shaq", email:"shaq@attack.com", password:"password")
+user_count = User.all.count
+user = User.find(rand(user_count)+1)
+Question.create(user_id: user, title: "is jason too into toilets???", body: "I mean the kid does seem a little weird about the whole thing....")
+Answer.create(body: "seems like it", user_id: user, question_id: 1)
+count = 1000
+puts "seeding #{count} questions."
+count.times do
+  user = User.find(rand(user_count)+1)
+  q = Question.create(user_id: user.id, title: "How do you "+ Faker::Hacker.verb + " a " + Faker::Hacker.noun+"?",
+   body: "My " + Faker::Hacker.noun + " is " + Faker::Hacker.verb + ".\n" + Faker::Lorem.sentence(random_words_to_add = 1)+"\n")
 
-
-4.times do
-  q = Question.create(user_id:1, title: Faker::Hacker.ingverb+"?", body: Faker::Hacker.say_something_smart+"???")
-  4.times do
-    Answer.create(question_id:q.id, user_id:2, body: Faker::Hacker.say_something_smart+"!")
+  8.times do
+    Answer.create(question_id:q.id, user_id: user.id, body: Faker::Hacker.say_something_smart+"!")
   end
 end
