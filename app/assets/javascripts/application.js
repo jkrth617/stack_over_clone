0b85ef227c47a90816751ebabaf1b75b77e80a7d
AgentWang007 <jaybwang007@gmail.com>
2015-10-25 02:21:24 -0400

    add mobile permissions so that the video doesn't start on mobile devicesautomatically

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,4 +33,40 @@ $(document).on('ready', function(){
     })
   })
 
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+
+if(isMobile.any()) {
+  } else {
+          var sources = document.querySelectorAll('video#vid-div source');
+          var video = document.querySelector('video#vid-div');
+          for(var i = 0; i<sources.length;i++) { sources[i].setAttribute('src', sources[i].getAttribute('data-src'));
+      }
+  video.load();
+}
+
+
 });
+
+
+
