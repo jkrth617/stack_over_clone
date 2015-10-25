2225b6145816d222af110783efffe578ef87fd1b
jake roth <email": null>
2015-10-25 12:39:23 -0400

    took out the sync behavior into its own method so that the create controller method doesnt get too cluttered. Adding Aync behavior now

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,24 +12,33 @@ class AnswersController < ApplicationController
   def create
     question = Question.find(params[:question_id])
     new_a = Answer.new(valid_params)
-      if current_user
-        if new_a.valid?
-          question.answers << new_a
-          redirect_to question
-        else
-          flash[:errors] = new_q.errors.messages.values.join("\n")
-          redirect_to new_question_answer_path(question)
-        end
-      else
-        flash[:errors] = "You must be logged in to add answers."
-        redirect_to new_question_answer_path
-      end
+    if request.xhr?
+      answer_creation_Async(question, new_a)
+    else
+      answer_creation_sync(question, new_a)
+    end
   end
 
   def vote
     parent_answer = Answer.find_by(id: params[:answer_id])
     make_vote(parent_answer)
   end
+
+  def answer_creation_sync(question, new_a)
+    if current_user
+      if new_a.valid?
+        question.answers << new_a
+        redirect_to question
+      else
+        flash[:errors] = new_q.errors.messages.values.join("\n")
+        redirect_to new_question_answer_path(question)
+      end
+    else
+      flash[:errors] = "You must be logged in to add answers."
+      redirect_to new_question_answer_path
+    end
+  end
+
 
   def valid_params
     params.require(:answer).permit(:body, :user_id, :question_id)
