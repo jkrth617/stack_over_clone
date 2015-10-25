0fe6c024cec52760d1e79d7bec0e326d5cbadda8
jake roth <email": null>
2015-10-25 12:43:45 -0400

    adding ajax but need to create a partial so that I can hand something back

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -24,6 +24,21 @@ class AnswersController < ApplicationController
     make_vote(parent_answer)
   end
 
+  def answer_creation_Async(question, new_a)
+    if current_user
+      if new_a.valid?
+        question.answers << new_a
+        render 'answers/_single-answer', layout: false
+      else
+        flash[:errors] = new_q.errors.messages.values.join("\n")
+        redirect_to new_question_answer_path(question)
+      end
+    else
+      flash[:errors] = "You must be logged in to add answers."
+      redirect_to new_question_answer_path
+    end
+  end
+
   def answer_creation_sync(question, new_a)
     if current_user
       if new_a.valid?
