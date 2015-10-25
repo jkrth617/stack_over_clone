c7982ac77ed6bfb951fdc0b1f152a27085495a9c
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-25 13:36:03 -0400

    add search to tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,13 @@ class Tag < ActiveRecord::Base
   has_many :question_tags, dependent: :destroy
   has_many :questions, through: :question_tags
   validates_presence_of :description
+
+  def self.search(search)
+    if search
+      where 'name LIKE ?', "%#{search}%"
+    else
+      #something 
+    end
+  end
 end
+
