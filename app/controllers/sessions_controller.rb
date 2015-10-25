b047cc5b87fe78575a0ac56cd8b7b10153767016
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-25 17:14:30 -0400

    remove notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    binding.pry
     session[:user_id] = user.id
     redirect_to root_url
   end
