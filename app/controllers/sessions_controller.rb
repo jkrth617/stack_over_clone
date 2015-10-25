389b721fc1d00d20c66859862e72c4caaff73df2
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-25 15:58:05 -0400

    add omniauth to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,13 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
+  end
+
   def logout
     session.clear
     flash[:message] = "Successfully logged out"
