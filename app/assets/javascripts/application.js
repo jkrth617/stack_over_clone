e8756621e5d0a9aca818b6b24ed64b03274638e4
AgentWang007 <jaybwang007@gmail.com>
2015-10-25 04:59:03 -0400

    add sticky navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,6 +33,26 @@ $(document).on('ready', function(){
     })
   })
 
+$(document).ready(function() {
+  var stickyNavTop = $('.nav').offset().top;
+
+  var stickyNav = function(){
+  var scrollTop = $(window).scrollTop();
+
+  if (scrollTop > stickyNavTop) {
+      $('.nav').addClass('sticky');
+  } else {
+      $('.nav').removeClass('sticky');
+  }
+  };
+
+  stickyNav();
+
+  $(window).scroll(function() {
+      stickyNav();
+  });
+  });
+
 
 var isMobile = {
     Android: function() {
