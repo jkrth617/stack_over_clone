a3d94f4220d163c87dc542f7d74352c422b22121
smokecode <smokecode@gmail.com>
2015-10-25 17:12:17 -0400

    curly brace removed from bottom of app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -76,13 +76,13 @@ $(document).on('ready', function(){
     var stickyNavTop = $('.nav').offset().top;
 
     var stickyNav = function(){
-    var scrollTop = $(window).scrollTop();
+      var scrollTop = $(window).scrollTop();
 
-    if (scrollTop > stickyNavTop) {
-        $('.nav').addClass('sticky');
-    } else {
-        $('.nav').removeClass('sticky');
-    }
+      if (scrollTop > stickyNavTop) {
+          $('.nav').addClass('sticky');
+      } else {
+          $('.nav').removeClass('sticky');
+      }
     };
 
     stickyNav();
