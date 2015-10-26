309c25843bd70899efbcb0130df2f51228037f43
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-25 22:00:21 -0400

    remove binding.pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,6 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    binding.pry
     session[:user_id] = user.id
     redirect_to root_url
   end
