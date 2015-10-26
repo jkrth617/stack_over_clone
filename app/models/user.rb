b77dbdc9aae2fdfd2c5a80c0af3cb768be6f3949
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-25 21:13:33 -0400

    uncomment oauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,18 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :votes
 
-  validates_presence_of :username, :email, :password_digest#, unless: -> { from_omniauth? }
+  validates_presence_of :username, :email, :password_digest, unless: -> { from_omniauth? }
   validates_uniqueness_of :email, :username
   #validates :email, format: { }
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.username = auth["info"]["name"]
+      user.name = auth["info"]["name"]
     end
   end
   
-  #def from_omniauth?
-   # provider && uid 
- # end
+  def from_omniauth?
+    provider && uid 
+  end
 end
